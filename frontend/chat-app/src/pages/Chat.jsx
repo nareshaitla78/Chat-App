eb99b4f56981d13e87cd4a327e359c1a881a18d2
nareshaitla78 <nareshaitla1122.com>
2025-01-10 11:56:38 +0530

    removed localhost:5000 added render deployed link

--- frontend/chat-app/src/pages/Chat.jsx
+++ frontend/chat-app/src/pages/Chat.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Chat() {
     return (
         <div>
-        <h1>I love You neha Sri</h1>
-        
+        <Link to="/login">Login</Link>
+        <Link to="/register">Login</Link>   
         </div>
     );
     }
--- frontend/chat-app/src/utils/APIRoutes.js
+++ frontend/chat-app/src/utils/APIRoutes.js
@@ -1,3 +1,3 @@
-const host="http://localhost:5000";
+const host="https://chat-app-e2vq.onrender.com";
 export const registerRoute=`${host}/api/auth/register`;
 export const LoginRoute=`${host}/api/auth/Login`;
