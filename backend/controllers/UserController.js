959dcdb8d89a206fff6af7f2bd9edf10487dfa24
nareshaitla78 <nareshaitla1122.com>
2025-01-11 07:16:51 +0530

    minor fix

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -71,7 +71,7 @@ module.exports.resetPassword = async (req, res) => {
       user.resetTokenExpiration = resetTokenExpiration;
   
       await user.save();
-      const resetLink = `${process.env.HOST}/reset-password/${resetToken}`;
+      const resetLink = `/reset-password/${resetToken}`;
       console.log(process.env.HOST,resetLink,"hosttttttttttttttttttttt",process.env.EMAIL_USER,process.env.SENDGRID_API_KEY);
       
       const htmlContent = `
